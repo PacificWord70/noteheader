e042343832802150308cbbf1d1495f7c8c8101fe
PacificWord70 <heidianderson@sfhs.com>
2017-06-21 15:54:26 -0400

    sidebar new button makes new note

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import './Main.css'
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-import './Main.css'
-
 const Main = (props) => {
   return (
     <div className="Main">
-      <Sidebar resetCurrentNote={props.resetCurrentNote} signOut={props.signOut}/>
-      <NoteList notes={props.notes} setCurrentNote={props.setCurrentNote}/>
+      <Sidebar signOut={props.signOut} />
+      <NoteList
+        notes={props.notes}
+        setCurrentNote={props.setCurrentNote}
+      />
+
       <Switch>
-        <Route path="/notes/:id" render={(navProps)=>(
+        <Route path="/notes/:id" render={(navProps) => (
           <NoteForm {...props} {...navProps} />
-        )}/>
-        <Route path="/notes" render={(navProps)=>(
+        )} />
+        <Route path="/notes" render={(navProps) => (
           <NoteForm {...props} {...navProps} />
-        )}/>
+        )} />
       </Switch>
     </div>
-  );
-  
+  )
 }
 
-export default Main;
+export default Main
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,48 +1,66 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
 import './NoteForm.css'
 
 class NoteForm extends Component {
-    handleChanges = (ev) => {
-        const note = {...this.props.currentNote}
-        note[ev.target.name] = ev.target.value
-        this.props.saveNote(note)
-    }
+  componentWillReceiveProps(nextProps) {
+    const newId = nextProps.match.params.id
 
-    handleRemove = (ev) => {
-        this.props.removeNote(this.props.currentNote)
+    if (newId) {
+      if (newId !== this.props.currentNote.id) {
+        const note = nextProps.notes[newId]
+        if (note) {
+          this.props.setCurrentNote(note)
+        } else if (Object.keys(nextProps.notes).length > 0) {
+          this.props.history.push('/notes')
+        }
+      }
+    } else if (this.props.currentNote.id) {
+      this.props.resetCurrentNote()
     }
+  }
 
-    render(){
-        return (
-            <div className="NoteForm">
-                <form>
-                    <p>
-                        <input 
-                            type="text" 
-                            name="title" 
-                            placeholder="Title your note" 
-                            onChange={this.handleChanges}
-                            value={this.props.currentNote.title}
-                        />
-                    </p>
-                    <p>
-                        <textarea 
-                            name="body" 
-                            placeholder="Just start typing..."
-                            onChange={this.handleChanges}
-                            value={this.props.currentNote.body}
-                        ></textarea>
-                    </p>
-                    <button
-                        type="button"
-                        onClick={this.handleRemove}
-                    >
-                        <i className="fa fa-trash-o">Delete</i>
-                    </button>
-                </form>
-            </div>
-        )
-    }
+  handleChanges = (ev) => {
+    const note = {...this.props.currentNote}
+    note[ev.target.name] = ev.target.value
+    this.props.saveNote(note)
+  }
+
+  handleRemove = (ev) => {
+    this.props.removeNote(this.props.currentNote)
+  }
+
+  render() {
+    return (
+      <div className="NoteForm">
+        <form>
+          <p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title your note"
+              onChange={this.handleChanges}
+              value={this.props.currentNote.title}
+            />
+          </p>
+          <p>
+            <textarea
+              name="body"
+              placeholder="Just start typing..."
+              onChange={this.handleChanges}
+              value={this.props.currentNote.body}
+            ></textarea>
+          </p>
+          <button
+           type="button"
+           onClick={this.handleRemove}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default NoteForm
